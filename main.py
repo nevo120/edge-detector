369d28aa30819c1c71d726b9c53c12369376fb93
Nevo Hazan <nevo6789@gmail.com>
2026-08-27 12:11:58 +0300

    add thershold based edge detecction

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 pixels = [10,12,11,15,210,215,218]
+threshold = 50
 for i in range(len(pixels)-1):
     difference = abs(pixels[i+1] - pixels[i])
     print (difference)
+    if difference > threshold:
+        print(f"edge detected between pixels {i} and {i+1}")
+
+
+    
